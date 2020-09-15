b12c6c3ef176b4f6458475fdad868704fc52f74c
Joshua Carey <jdcarey128@gmail.com>
2020-09-15 13:08:13 -0600

    Set up item initialize and attr readers for name and price

---
+++ test/food_truck_test.rb
@@ -0,0 +1,11 @@
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+end
---
+++ test/item_test.rb
@@ -0,0 +1,14 @@
+require './lib/item'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    assert_instance_of Item, item1
+    assert_equal 'Peach Pie (Slice)', item1.name
+    assert_equal "$3.75", item1.price
+  end
+
+end
