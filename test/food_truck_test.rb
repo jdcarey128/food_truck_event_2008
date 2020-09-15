1c4bf9e8cc1de74b3df5ae74b347ed11a33f8d04
Joshua Carey <jdcarey128@gmail.com>
2020-09-15 13:19:43 -0600

    Add initialize and attr readers for food truck

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -8,6 +8,13 @@ class FoodTruckTest < Minitest::Test
   def setup
     @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_instance_of FoodTruck, @food_truck
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal Hash.new, @food_truck.inventory
   end
 
 
