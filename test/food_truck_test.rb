42a6e7a25f57a69298d1762eef12daec66dd36cc
Joshua Carey <jdcarey128@gmail.com>
2020-09-15 13:24:31 -0600

    Structure inventory test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -17,5 +17,23 @@ class FoodTruckTest < Minitest::Test
     assert_equal Hash.new, @food_truck.inventory
   end
 
+  def test_it_can_add_items_to_and_read_from_inventory
+    assert_equal 0, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 30)
+    expected = {
+      @item1 => 30
+    }
+    assert_equal expected, @food_truck.inventory
+    assert_equal 30, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item2, 12)
+    expected_2 = {
+      @item1 => 55,
+      @item2 => 12
+    }
+    assert_equal expected_2, @food_truck.inventory
+  end
+
 
 end
